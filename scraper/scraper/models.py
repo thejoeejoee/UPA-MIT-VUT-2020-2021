aafb768e5eb82ecc6dcd06e0fe2081cbb6c34db8
Vojtech Hertl <xhertl04@stud.fit.vutbr.cz>
2020-10-24 19:51:56 +0200

    added input parsing and proper mongodb storing

--- scraper/scraper/models.py
+++ scraper/scraper/models.py
@@ -1,6 +1,43 @@
-from mongoengine import DynamicDocument
+from mongoengine import Document, ReferenceField, DictField, StringField, FloatField, DateTimeField, IntField
 
 
-# TODO: define document for measurement
-class Station(DynamicDocument):
-    pass
+class Station(Document):
+    wmo_id = IntField(primary_key=True)
+    location = StringField()
+    station_name = StringField()
+    station_height = FloatField()
+    latitude = FloatField()
+    longitude = FloatField()
+
+
+class Measurement(Document):
+    station = ReferenceField(Station, required=True)
+    time_period = DateTimeField()
+    apparent_temp = FloatField()
+    cloud = StringField()
+    cloud_oktas = IntField()
+    cloud_type_id = IntField()
+    delta_t = FloatField()
+    air_temperature = FloatField()
+    dew_point = FloatField()
+    pres = FloatField()
+    msl_pres = FloatField()
+    qnh_pres = FloatField()
+    rain_hour = FloatField()
+    rain_ten = FloatField()
+    rel_humidity = IntField()
+    vis_km = FloatField()
+    weather = StringField()
+    wind_dir = StringField()
+    wind_dir_deg = IntField()
+    wind_spd_kmh = IntField()
+    wind_spd = IntField()
+    gust_kmh = IntField()
+    wind_gust_spd = IntField()
+    rainfall = DictField()
+    rainfall_24hr = DictField()
+    maximum_air_temperature = DictField()
+    minimum_air_temperature = DictField()
+    maximum_gust_spd = DictField()
+    maximum_gust_kmh = DictField()
+    maximum_gust_dir = DictField()
--- scraper/scraper/scraper.py
+++ scraper/scraper/scraper.py
@@ -9,7 +9,7 @@ from decouple import AutoConfig
 from pymongo import MongoClient
 
 from . import logger
-from .models import Station
+from .models import Station, Measurement
 
 
 class Scraper:
@@ -95,8 +95,93 @@ class Scraper:
     def _store_data(self, file_content: str):
         data = xmltodict.parse(file_content)
 
-        # TODO: find best way to unwind measurements
-        station = Station()
-        station.data = data
+        stations = data.get("product").get("observations").get("station")
+        for station_data in stations:
+            wmo_id = int(station_data.get("@wmo-id"))
 
-        station.save()
+            # checking existence of station
+            fetched_station = Station.objects(wmo_id=wmo_id)  # returns QuerySet
+            if not fetched_station:
+                station = Station()
+                station.wmo_id = wmo_id
+                station.location = station_data.get("@tz")
+                station.station_name = station_data.get("@stn-name")
+                station.station_height = float(station_data.get("@stn-height"))
+                station.latitude = float(station_data.get("@lat"))
+                station.longitude = float(station_data.get("@lon"))
+                station.save()
+            else:
+                station = fetched_station.first()
+
+            measurement = Measurement()
+            measurement.station = station.to_dbref()
+            period = station_data.get("period")
+            measurement.time_period = period.get("@time-utc")
+
+            elements = period.get("level").get("element")
+            if elements is None:
+                break
+            for element in elements:
+                if type(element) == str:  # should probably check if element is OrderedDict
+                    break
+                element_type = element.get("@type")
+                element_value = element.get("#text")
+                if element_type == "time_period":
+                    measurement.time_period = element_value
+                elif element_type == "apparent_temp":
+                    measurement.apparent_temp = float(element_value)
+                elif element_type == "cloud":
+                    measurement.cloud = element_value
+                elif element_type == "cloud_oktas":
+                    measurement.cloud_oktas = int(element_value)
+                elif element_type == "cloud_type_id":
+                    measurement.cloud_type_id = int(element_value)
+                elif element_type == "delta_t":
+                    measurement.delta_t = float(element_value)
+                elif element_type == "air_temperature":
+                    measurement.air_temperature = float(element_value)
+                elif element_type == "dew_point":
+                    measurement.dew_point = float(element_value)
+                elif element_type == "pres":
+                    measurement.pres = float(element_value)
+                elif element_type == "msl_pres":
+                    measurement.msl_pres = float(element_value)
+                elif element_type == "qnh_pres":
+                    measurement.qnh_pres = float(element_value)
+                elif element_type == "rain_hour":
+                    measurement.rain_hour = float(element_value)
+                elif element_type == "rain_ten":
+                    measurement.rain_ten = float(element_value)
+                elif element_type == "rel_humidity":
+                    measurement.rel_humidity = int(element_value)
+                elif element_type == "vis_km":
+                    measurement.vis_km = float(element_value)
+                elif element_type == "weather":
+                    measurement.weather = element_value
+                elif element_type == "wind_dir":
+                    measurement.wind_dir = element_value
+                elif element_type == "wind_dir_deg":
+                    measurement.wind_dir_deg = int(element_value)
+                elif element_type == "wind_spd_kmh":
+                    measurement.wind_spd_kmh = int(element_value)
+                elif element_type == "wind_spd":
+                    measurement.wind_spd = int(element_value)
+                elif element_type == "gust_kmh":
+                    measurement.gust_kmh = int(element_value)
+                elif element_type == "wind_gust_spd":
+                    measurement.wind_gust_spd = int(element_value)
+                elif element_type == "rainfall":
+                    measurement.rainfall = element
+                elif element_type == "rainfall_24hr":
+                    measurement.rainfall_24hr = element
+                elif element_type == "maximum_air_temperature":
+                    measurement.maximum_air_temperature = element
+                elif element_type == "minimum_air_temperature":
+                    measurement.minimum_air_temperature = element
+                elif element_type == "maximum_gust_spd":
+                    measurement.maximum_gust_spd = element
+                elif element_type == "maximum_gust_kmh":
+                    measurement.maximum_gust_kmh = element
+                elif element_type == "maximum_gust_dir":
+                    measurement.maximum_gust_dir = element
+            measurement.save()
