6c79c4901a0f5fc082b380fc2eeef5c29cdd06da
Josef Kolář <thejoeejoee@gmail.com>
2020-10-24 13:20:27 +0200

    Added last-modification datetime resolving to scraper

--- scraper/scraper/scraper.py
+++ scraper/scraper/scraper.py
@@ -1,8 +1,10 @@
 import os
+from datetime import datetime
 from ftplib import FTP
-from os import makedirs
+from os import makedirs, stat
 
 import xmltodict
+from dateutil import parser
 from decouple import AutoConfig
 from pymongo import MongoClient
 
@@ -26,8 +28,7 @@ class Scraper:
     def run(self):
         local_storage_dir = self._config('LOCAL_STORAGE_DIR')
 
-        # TODO: remove after ftp.stat is implemented (bellow)
-        scraped = self._scrape(skip_download=False)
+        scraped = self._scrape()
 
         makedirs(local_storage_dir, exist_ok=True)
 
@@ -35,7 +36,7 @@ class Scraper:
             with open(os.path.join(local_storage_dir, f), 'r') as fd:
                 self._store_data(file_content=fd.read())
 
-    def _scrape(self, skip_download=False):
+    def _scrape(self):
         host = self._config('FTP_HOST')
         files_to_scrape = set(self._config('FILES_TO_SCRAPE').split())
         dir_to_scrape = self._config('FTP_DIR')
@@ -54,16 +55,42 @@ class Scraper:
 
             logger.debug('Found %s files to scrape: %s.', len(files_to_scrape), ','.join(found_to_scrape))
 
-            if skip_download:
-                return found_to_scrape
-
-            # TODO: scrape only modified files (smth like ftp.stat?)
             for i, to_scrape in enumerate(found_to_scrape, start=1):
-                with open(os.path.join(local_storage_dir, to_scrape), 'wb') as local_fp:
+                local_file_pth = os.path.join(local_storage_dir, to_scrape)
+
+                last_modified_remote = self.remote_file_last_modified(ftp, to_scrape)
+                last_modified_local = self._local_file_last_modified(local_file_pth)
+
+                if last_modified_remote and last_modified_local and last_modified_local > last_modified_remote:
+                    logger.debug(
+                        'File %s: %s skipped (last modified difference %s).',
+                        i, to_scrape, last_modified_local - last_modified_remote
+                    )
+                    continue
+
+                with open(local_file_pth, 'wb') as local_fp:
                     ftp.retrbinary(f'RETR {to_scrape}', local_fp.write)
 
                 logger.debug('File %s: %s has been scraped.', i, to_scrape)
             return found_to_scrape
+
+    @staticmethod
+    def remote_file_last_modified(ftp: FTP, file_name: str):
+        modified = ftp.voidcmd(f'MDTM {file_name}')[4:].strip()
+
+        try:
+            return parser.parse(modified)
+        except (ValueError, OverflowError):
+            return None
+
+    @staticmethod
+    def _local_file_last_modified(file_name: str):
+        try:
+            stats = stat(file_name)
+        except FileNotFoundError:
+            return None
+
+        return datetime.fromtimestamp(stats.st_mtime)
 
     def _store_data(self, file_content: str):
         data = xmltodict.parse(file_content)
