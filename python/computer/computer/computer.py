49be789fb93a0381efe50ead54862521537d02b2
thalas <timotejhalas@gmail.com>
2020-11-19 11:47:11 +0100

    Create some mongodb pipelines and update gitignore

--- python/computer/computer/computer.py
+++ python/computer/computer/computer.py
@@ -7,7 +7,7 @@ class Computer(object):
     def run(self):
         Syncer.sync_stations()
 
-        pipeline = [
+        average_day_temperature_pipeline = [
             {
                 "$match": {
                     "air_temperature": {"$ne": None},
@@ -47,8 +47,110 @@ class Computer(object):
                 "$sort": {"_id.day": 1, "_id.station_id": 1},
             },
         ]
-        data = MeasurementDocument.objects().aggregate(pipeline)
 
+        tempereture_ranking_pipeline = [
+            {
+                "$match": {
+                    "air_temperature": {"$ne": None},
+                },
+
+            },
+            {
+                "$lookup":
+                    {
+                        "from": 'station',
+                        "localField": 'station',
+                        "foreignField": '_id',
+                        "as": 'station',
+                    },
+            },
+            {
+                "$unwind": {"path": '$station'},
+            },
+            {
+                "$project": {
+                    "station_id": "$station._id",
+                    "air_temperature": "$air_temperature",
+                },
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "station_id": "$station_id",
+                    },
+
+                    "avg_air_temperature": {"$avg": "$air_temperature"},
+                },
+            },
+            {
+                "$sort": {"avg_air_temperature": 1},
+            },
+        ]
+
+        rainfall_ranking_pipeline = [
+            {
+                "$match": {
+                    "rainfall_24hr": {"$ne": None},
+                },
+
+            },
+            {
+                "$lookup":
+                    {
+                        "from": 'station',
+                        "localField": 'station',
+                        "foreignField": '_id',
+                        "as": 'station',
+                    },
+            },
+            {
+                "$unwind": {"path": '$station'},
+            },
+            {
+                "$project": {
+                    "station_id": "$station._id",
+                    "day": {"$dateToString": {"format": "%Y-%m-%d", "date": "$time_period"}},
+                    "time": {"$dateToString": {"format": "%H:%M:%S", "date": "$time_period"}},
+                    "history_rainfall": { "$ifNull": [ { "$toDouble": "$rainfall_24hr.#text" }, 0.0]} ,
+                    "actual_rainfall": { "$ifNull": [ { "$toDouble": "$rainfall.#text" }, 0.0]},
+                },
+            },
+            {
+                "$sort": {"station_id": 1, "day": 1, "time": 1},
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "station_id": "$station_id",
+                        "day": "$day",
+                    },
+
+                    "time": {"$last": "$time"},
+                    "history_rainfall": {"$last": "$history_rainfall"},
+                    "actual_rainfall": {"$last": "$actual_rainfall"},
+                },
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "station_id": "$_id.station_id",
+                    },
+                    "all_history_rainfalls": {"$sum": "$history_rainfall"},
+                    "last_rainfall": {"$last": "$actual_rainfall"},
+                },
+            },
+            {
+                "$project": {
+                    "_id": "$_id",
+                    "rainfalls": { "$add": [ "$all_history_rainfalls", "$last_rainfall" ] },
+                },
+            },
+            {
+                "$sort": {"rainfalls": 1},
+            },
+        ]
+
+        data = MeasurementDocument.objects().aggregate(average_day_temperature_pipeline)
         for item in data:
             print(item)
 
